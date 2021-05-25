cc987efa59a2676921b7aa05dba9c61428a92fbe
andreaswolff <wandrease@gmail.com>
2021-05-25 10:27:21 +0300

    added update and delete functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Wine = require("./models/wine");
+const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/velvetvine", {
   useNewUrlParser: true,
@@ -19,6 +20,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -27,6 +30,38 @@ app.get("/", (req, res) => {
 app.get("/wines", async (req, res) => {
   const wines = await Wine.find({});
   res.render("wines/index", { wines });
+});
+
+app.get("/wines/new", (req, res) => {
+  res.render("wines/new");
+});
+
+app.post("/wines", async (req, res) => {
+  const wine = new Wine(req.body.wine);
+  await wine.save();
+  res.redirect(`/wines/${wine._id}`);
+});
+
+app.get("/wines/:id", async (req, res) => {
+  const wine = await Wine.findById(req.params.id);
+  res.render("wines/show", { wine });
+});
+
+app.get("/wines/:id/edit", async (req, res) => {
+  const wine = await Wine.findById(req.params.id);
+  res.render("wines/edit", { wine });
+});
+
+app.put("/wines/:id", async (req, res) => {
+  const { id } = req.params;
+  const wine = await Wine.findByIdAndUpdate(id, { ...req.body.wine });
+  res.redirect(`/wines/${wine._id}`);
+});
+
+app.delete("/wines/:id", async (req, res) => {
+  const { id } = req.params;
+  await Wine.findByIdAndDelete(id);
+  res.redirect("/wines");
 });
 
 app.listen(3000, (req, res) => {
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 # Velvetvine
 
-I built this project for my personal portofolio.
+I built this project for my personal portofolio.<br />
 It was built using CRUD principles, MongoDB, ExpressJS, Embedded JavaScript and Tailwind for styling purposes.
